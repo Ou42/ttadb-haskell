f3078f56f708a23fd86317d758b314f24f4bb864
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-10-16 21:36:46 -0700

    made CRUD

--- app/Main.hs
+++ app/Main.hs
@@ -5,41 +5,70 @@
 
 module Main where
 
+import Prelude hiding (id)
 import qualified Data.Text as Text
 import qualified Web.Scotty as Scotty
 import qualified Database.SQLite.Simple as DB
+import Database.SQLite.Simple (NamedParam(..))
 import Database.SQLite.Simple.QQ (sql)
 import qualified Data.Text.Internal.Builder as Text
 import qualified Data.Text.Lazy as Text.Lazy
 import Database.SQLite.Simple.FromRow (FromRow)
 import GHC.Generics (Generic)
 import qualified Network.HTTP.Types.Status as Status
+import qualified Text.Blaze.Html
+import qualified Text.Blaze.Html5 as HTML
 
-data Greeting = Greeting { id :: Int, greeting :: Text.Text }
+data ToDo = ToDo { id :: Int, todo :: Text.Text }
   deriving (Generic, FromRow, Show)
 
 main :: IO ()
 main = DB.withConnection "ttadb.db" $ \conn -> do
     -- conn <- DB.open "ttadb.db" -- with conn ( later )
 
-    DB.execute_ conn [sql|create table if not exists greetings
+    DB.execute_ conn [sql|create table if not exists todos
                           ( id integer primary key autoincrement
-                          , greeting text
-                          );|]
+                          , todo text
+                          );|] -- between [sql| ... |] is a quasi-quoter, this is SQL not Haskell
 
     Scotty.scotty 4242 $ do
 
         Scotty.get "/" $ do
             -- Scotty.html "<h1>hello!</h1>"
-            greetings <- Scotty.liftAndCatchIO $
-                         DB.query_ conn [sql|select id, greeting from greetings;|] :: Scotty.ActionM [Greeting]
-            Scotty.html $ "<h1>" <> Text.Lazy.pack (show greetings) <> "</h1>"
+            todos <- Scotty.liftAndCatchIO $
+                         DB.query_ conn [sql|select id, todo from todos;|] :: Scotty.ActionM [ToDo]
+            Scotty.html $ "<h1>" <> Text.Lazy.pack (show todos) <> "</h1>"
 
         Scotty.post "/" $ do
-            greeting <- Scotty.param "greeting"
+            todo <- Scotty.param "todo"
             Scotty.liftAndCatchIO $
-                        DB.execute conn [sql|insert into greetings (greeting) values (?);|] (DB.Only greeting :: DB.Only Text.Text)
+                DB.execute conn [sql|insert into todos (todo) values (?);|] (DB.Only todo :: DB.Only Text.Text)
 
-            Scotty.status Status.status200
+            -- Scotty.status Status.status200
+
+        Scotty.post "/:id" $ do
+            id <- Scotty.param "id"
+            todo <- Scotty.param "todo"
+            Scotty.liftAndCatchIO $
+                DB.executeNamed conn [sql|update todos set todo=:todo where id=:id;|]
+                    [ ":id" := (id :: Int), ":todo" := (todo :: Text.Text) ]
+
+        Scotty.delete "/:id" $ do
+            id <- Scotty.param "id"
+            Scotty.liftAndCatchIO $
+                DB.executeNamed conn [sql|delete from todos where id=:id ;|]
+                    [ ":id" := (id :: Int) ]
+
+        Scotty.get "/:id" $ do
+            id <- Scotty.param "id"
+            todo <- Scotty.liftAndCatchIO $
+                DB.queryNamed conn [sql|select * from todos where id=:id;|]
+                    [ ":id" := (id :: Int) ] :: Scotty.ActionM [ToDo]
+            if null todo
+                then
+                    Scotty.status Status.status404
+                else
+                    Scotty.html $ "<h1>" <> Text.Lazy.pack (show todo) <> "</h1>"
+
 
     -- DB.close conn
