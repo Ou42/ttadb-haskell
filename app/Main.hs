24071371ab956473e6044ac441b97d7f11165494
Ou42 <25629874+Ou42@users.noreply.github.com>
2024-02-05 08:10:07 -0800

    handling exceptions / run-time errors WIP - update 2
    - compiles
    - runs
    - still errors out
    - but now there are messages in the terminal
    - was previously using a dated version of Scotty's exceptions.hs example

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,23 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
+import Control.Exception (Exception(..))
+import Control.Monad.IO.Class ( MonadIO )
+import Control.Monad.IO.Unlift (MonadUnliftIO(..))
+import Data.String (fromString)
+import Data.Typeable ( Typeable )
+import qualified Network.Wai.Middleware.RequestLogger as MW
+
+import qualified Web.Scotty.Trans as ScottyT
+
 import Prelude hiding (id)
--- import Control.Monad.IO.Class
+import qualified Prelude
 import qualified Data.Text as Text
 import qualified Web.Scotty as Scotty
--- import Web.Scotty.Trans
 import qualified Database.SQLite.Simple as DB
 import Database.SQLite.Simple (NamedParam(..))
 import Database.SQLite.Simple.QQ (sql)
@@ -35,31 +44,22 @@ data ToDo = ToDo { id :: Int
                  }
   deriving (Generic, FromRow, Show)
 
--- Define a custom exception type.
-{- data Except = Forbidden | NotFound Int | StringEx String
-    deriving (Show, Eq)
--}
+-- | A custom exception type.
+data Except = Forbidden | NotFound Int | StringEx String
+    deriving (Show, Eq, Typeable, Exception)
 
--- The type must be an instance of 'ScottyError'.
--- 'ScottyError' is essentially a combination of 'Error' and 'Show'.
-{- instance ScottyError Except where
-instance ScottyException Except where
-    stringError = StringEx
-    showError = fromString . show
--}
-
--- Handler for uncaught exceptions.
-{- handleEx :: Monad m => Except -> ActionT Except m ()
-handleEx Forbidden    = do
-    status status403
-    html "<h1>Scotty Says No</h1>"
-handleEx (NotFound i) = do
-    status status404
-    html $ fromString $ "<h1>Can't find " ++ show i ++ ".</h1>"
-handleEx (StringEx s) = do
-    status status500
-    html $ fromString $ "<h1>" ++ s ++ "</h1>"
--}
+-- | User-defined exceptions should have an associated Handler:
+handleEx :: MonadIO m => ScottyT.ErrorHandler m
+handleEx = ScottyT.Handler $ \case
+  Forbidden -> do
+    ScottyT.status Status.status403
+    ScottyT.html "<h1>Scotty Says No</h1>"
+  NotFound i -> do
+    ScottyT.status Status.status404
+    ScottyT.html $ fromString $ "<h1>Can't find " ++ show i ++ ".</h1>"
+  StringEx s -> do
+    ScottyT.status Status.status500
+    ScottyT.html $ fromString $ "<h1>" ++ s ++ "</h1>"
 
 updateForm1 :: ToDo -> HTML.Html
 -- updateForm1 ToDo {id, todo} =
@@ -93,127 +93,135 @@ main = do
                             , done_date TEXT
                             );|] -- between [sql| ... |] is a quasi-quoter, this is SQL not Haskell
 
-        Scotty.scotty port $ do
+        ScottyT.scottyT port Prelude.id (server conn jsFile cssFile) -- note: we use 'id' since we don't have to run any effects at each action
 
-            Scotty.get "/" $ do
-                todos <- Scotty.liftIO $
-                            -- DB.query_ conn [sql|select id, todo from todos;|] :: Scotty.ActionM [ToDo]
-                            DB.query_ conn [sql|select * from todos;|] :: Scotty.ActionM [ToDo]
+-- Any custom monad stack will need to implement 'MonadUnliftIO'
+-- server :: MonadUnliftIO m => ScottyT.ScottyT m ()
+server conn jsFile cssFile = do
+    ScottyT.middleware MW.logStdoutDev
 
+    ScottyT.defaultHandler handleEx    -- define what to do with uncaught exceptions
+
+-- Scotty.scotty port $ do
+
+    ScottyT.get "/" $ do
+        todos <- ScottyT.liftIO $
+                    DB.query_ conn [sql|select id, todo from todos;|] :: Scotty.ActionM [ToDo]
+                    -- DB.query_ conn [sql|select * from todos;|] :: Scotty.ActionM [ToDo]
+
+        Scotty.html $ renderHtml $ HTML.docTypeHtml $ do
+            HTML.head $ do
+                HTML.title "Talk to a Database | To-Do's"
+
+                noFavIcon
+
+                HTML.style $ HTML.toMarkup cssFile
+
+                HTML.script $ do
+                    -- // JS funcs called when buttons clicked
+                    HTML.toMarkup jsFile
+
+            HTML.body $ do
+                HTML.h1 "To-Do's"
+
+                HTML.ul $ do
+                    -- for_ todos $ \ToDo {id, todo} -> do
+                    for_ todos $ \ToDo {id, todo, done, done_date} -> do
+                        HTML.li ! Attributes.id ("todo-" <> HTML.toValue id) $ do
+                            HTML.div ! Attributes.class_ "flex-container" $ do
+
+                                HTML.a ! Attributes.name (HTML.toValue ("todo: " <> show id))
+                                        ! Attributes.href ("/" <> HTML.toValue id)
+                                        $ HTML.toMarkup todo
+
+                                HTML.button ! Attributes.value (HTML.toValue id)
+                                            ! Attributes.onclick "deleteToDo(this)"
+                                            $ "delete"
+
+                HTML.form ! Attributes.action "/" ! Attributes.method "post" $ do
+                    HTML.input ! Attributes.type_ "text" ! Attributes.name "todo"
+                    HTML.input ! Attributes.type_ "submit" -- calls post on "/"
+
+    Scotty.get "/admin" $ do
+        todos <- Scotty.liftIO $
+                    DB.query_ conn [sql|select id, todo, done, done_date from todos;|] :: Scotty.ActionM [ToDo]
+                    -- DB.query_ conn [sql|select id, todo from todos;|] :: Scotty.ActionM [ToDo]
+
+        Scotty.html $ renderHtml $ HTML.docTypeHtml $ do
+            HTML.head $ do
+                HTML.title "<< Admin Console >> Talk to a Database | To-Do's << Admin Console >>"
+
+                noFavIcon
+
+                HTML.style $ HTML.toMarkup cssFile
+
+                HTML.script $ do
+                    -- // JS funcs called when buttons clicked
+                    HTML.toMarkup jsFile
+
+            HTML.body $ do
+                HTML.h1 "To-Do's"
+
+                HTML.ul $ do
+                    for_ todos $ \ToDo {id, todo} -> do
+                        HTML.li ! Attributes.id ("todo-" <> HTML.toValue id) $ do
+                            HTML.div ! Attributes.class_ "flex-container" $ do
+
+                                HTML.a ! Attributes.name (HTML.toValue ("todo: " <> show id))
+                                        ! Attributes.href ("/" <> HTML.toValue id)
+                                        $ HTML.toMarkup todo
+
+                                HTML.button ! Attributes.value (HTML.toValue id)
+                                            ! Attributes.onclick "deleteToDo(this)"
+                                            $ "delete"
+
+                HTML.form ! Attributes.action "/" ! Attributes.method "post" $ do
+                    HTML.input ! Attributes.type_ "text" ! Attributes.name "todo"
+                    HTML.input ! Attributes.type_ "submit" -- calls post on "/"
+
+    Scotty.post "/" $ do
+        todo <- Scotty.formParam "todo"
+        -- Scotty.liftAndCatchIO $
+        Scotty.liftIO $
+            DB.execute conn [sql|insert into todos (todo) values (?);|] (DB.Only todo :: DB.Only Text.Text)
+
+        Scotty.redirect "/"
+
+    Scotty.post "/:id" $ do
+        id <- Scotty.captureParam "id"      -- capture is URL
+        todo <- Scotty.formParam "todo"  -- form is request body
+        Scotty.liftIO $
+            DB.executeNamed conn [sql|update todos set todo=:todo where id=:id;|]
+                [ ":id" := (id :: Int), ":todo" := (todo :: Text.Text) ]
+
+        Scotty.redirect ("/" <> Text.Lazy.pack (show id))
+
+
+    Scotty.delete "/:id" $ do
+        id <- Scotty.captureParam "id"
+        Scotty.liftIO $
+            DB.executeNamed conn [sql|delete from todos where id=:id ;|]
+                [ ":id" := (id :: Int) ]
+
+    Scotty.get "/:id" $ do
+        id <- Scotty.captureParam "id"
+        
+        todos <- Scotty.liftIO $
+            -- DB.queryNamed conn [sql|select * from todos where id=:id ;|]
+            DB.queryNamed conn [sql|select id, todo from todos where id=:id ;|]
+                [ ":id" := (id :: Int) ] :: Scotty.ActionM [ToDo]
+        if null todos
+            then
+                Scotty.status Status.status404
+            else
                 Scotty.html $ renderHtml $ HTML.docTypeHtml $ do
                     HTML.head $ do
-                        HTML.title "Talk to a Database | To-Do's"
+                        HTML.title $ mapM_ (HTML.toMarkup . todo) todos
 
                         noFavIcon
 
-                        HTML.style $ HTML.toMarkup cssFile
-
-                        HTML.script $ do
-                            -- // JS funcs called when buttons clicked
-                            HTML.toMarkup jsFile
-
                     HTML.body $ do
-                        HTML.h1 "To-Do's"
+                        HTML.h1 $ HTML.toMarkup (Text.pack "Editing: ")
+                                <> mapM_ (HTML.toMarkup . todo) todos
 
-                        HTML.ul $ do
-                            -- for_ todos $ \ToDo {id, todo} -> do
-                            for_ todos $ \ToDo {id, todo, done, done_date} -> do
-                                HTML.li ! Attributes.id ("todo-" <> HTML.toValue id) $ do
-                                    HTML.div ! Attributes.class_ "flex-container" $ do
-
-                                        HTML.a ! Attributes.name (HTML.toValue ("todo: " <> show id))
-                                               ! Attributes.href ("/" <> HTML.toValue id)
-                                               $ HTML.toMarkup todo
-
-                                        HTML.button ! Attributes.value (HTML.toValue id)
-                                                    ! Attributes.onclick "deleteToDo(this)"
-                                                    $ "delete"
-
-                        HTML.form ! Attributes.action "/" ! Attributes.method "post" $ do
-                            HTML.input ! Attributes.type_ "text" ! Attributes.name "todo"
-                            HTML.input ! Attributes.type_ "submit" -- calls post on "/"
-
-            Scotty.get "/admin" $ do
-                todos <- Scotty.liftIO $
-                            DB.query_ conn [sql|select id, todo, done, done_date from todos;|] :: Scotty.ActionM [ToDo]
-                            -- DB.query_ conn [sql|select id, todo from todos;|] :: Scotty.ActionM [ToDo]
-
-                Scotty.html $ renderHtml $ HTML.docTypeHtml $ do
-                    HTML.head $ do
-                        HTML.title "<< Admin Console >> Talk to a Database | To-Do's << Admin Console >>"
-
-                        noFavIcon
-
-                        HTML.style $ HTML.toMarkup cssFile
-
-                        HTML.script $ do
-                            -- // JS funcs called when buttons clicked
-                            HTML.toMarkup jsFile
-
-                    HTML.body $ do
-                        HTML.h1 "To-Do's"
-
-                        HTML.ul $ do
-                            for_ todos $ \ToDo {id, todo} -> do
-                                HTML.li ! Attributes.id ("todo-" <> HTML.toValue id) $ do
-                                    HTML.div ! Attributes.class_ "flex-container" $ do
-
-                                        HTML.a ! Attributes.name (HTML.toValue ("todo: " <> show id))
-                                               ! Attributes.href ("/" <> HTML.toValue id)
-                                               $ HTML.toMarkup todo
-
-                                        HTML.button ! Attributes.value (HTML.toValue id)
-                                                    ! Attributes.onclick "deleteToDo(this)"
-                                                    $ "delete"
-
-                        HTML.form ! Attributes.action "/" ! Attributes.method "post" $ do
-                            HTML.input ! Attributes.type_ "text" ! Attributes.name "todo"
-                            HTML.input ! Attributes.type_ "submit" -- calls post on "/"
-
-            Scotty.post "/" $ do
-                todo <- Scotty.formParam "todo"
-                -- Scotty.liftAndCatchIO $
-                Scotty.liftIO $
-                    DB.execute conn [sql|insert into todos (todo) values (?);|] (DB.Only todo :: DB.Only Text.Text)
-
-                Scotty.redirect "/"
-
-            Scotty.post "/:id" $ do
-                id <- Scotty.captureParam "id"      -- capture is URL
-                todo <- Scotty.formParam "todo"  -- form is request body
-                Scotty.liftIO $
-                    DB.executeNamed conn [sql|update todos set todo=:todo where id=:id;|]
-                        [ ":id" := (id :: Int), ":todo" := (todo :: Text.Text) ]
-
-                Scotty.redirect ("/" <> Text.Lazy.pack (show id))
-
-
-            Scotty.delete "/:id" $ do
-                id <- Scotty.captureParam "id"
-                Scotty.liftIO $
-                    DB.executeNamed conn [sql|delete from todos where id=:id ;|]
-                        [ ":id" := (id :: Int) ]
-
-            Scotty.get "/:id" $ do
-                id <- Scotty.captureParam "id"
-                
-                todos <- Scotty.liftIO $
-                    -- DB.queryNamed conn [sql|select * from todos where id=:id ;|]
-                    DB.queryNamed conn [sql|select id, todo from todos where id=:id ;|]
-                        [ ":id" := (id :: Int) ] :: Scotty.ActionM [ToDo]
-                if null todos
-                    then
-                        Scotty.status Status.status404
-                    else
-                        Scotty.html $ renderHtml $ HTML.docTypeHtml $ do
-                            HTML.head $ do
-                                HTML.title $ mapM_ (HTML.toMarkup . todo) todos
-
-                                noFavIcon
-
-                            HTML.body $ do
-                                HTML.h1 $ HTML.toMarkup (Text.pack "Editing: ")
-                                        <> mapM_ (HTML.toMarkup . todo) todos
-
-                                mapM_ updateForm1 todos
- 
+                        mapM_ updateForm1 todos
